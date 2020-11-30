d23ab25a0a8005c5275e157a01ba8970e3914787
Christian Simon <csimon1127@gmail.com>
2020-11-30 22:48:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,6 +64,16 @@ class Student
     end
     
   end
+  
+  def first_student_in_grade_10
+    sql = <<-SQL 
+    SELECT * 
+    FROM students
+    WHERE 
+    
+    SQL
+    
+  end 
 
   def self.find_by_name(name)
     # find the student in the database given a name
