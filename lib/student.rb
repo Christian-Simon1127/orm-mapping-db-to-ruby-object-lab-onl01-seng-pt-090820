9afbcf821647e00e57e0ec4b925ef71a72290180
Christian Simon <csimon1127@gmail.com>
2020-11-30 23:06:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,17 +66,7 @@ class Student
   end
   
   def self.first_student_in_grade_10
-    sql = <<-SQL 
-    SELECT * 
-    FROM students
-    WHERE grade = 10
-    GROUP BY students.id
-    LIMIT 1;
-    SQL
-    
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    self.first_X_students_in_grade_10(1)
     
   end 
 
